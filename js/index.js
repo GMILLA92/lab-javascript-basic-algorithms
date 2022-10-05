183e377521e0b6874ea1f942f4eab48558562c1a
Gemma Milla <gmilla@Gemmas-MacBook-Air.local>
2022-10-05 17:35:43 +0200

    done with one bonus version 2

--- js/index.js
+++ js/index.js
@@ -71,6 +71,7 @@ if (found === false) {
 
 //Bonus1
 const newString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt sagittis leo eget facilisis. Donec eget leo tortor. Vivamus quis elementum ante, vel vulputate augue. Duis bibendum sem quis massa aliquam fermentum. Sed feugiat luctus vestibulum. Vivamus sed sapien sodales, dignissim purus non, efficitur odio. Nunc vel rutrum lacus. Nam viverra ipsum sed nulla elementum, eu porta mi elementum. Nam tempus a odio id mollis. Pellentesque tortor risus, blandit at tincidunt a, feugiat non quam. Integer eu quam imperdiet, maximus metus vitae, sodales enim. Fusce a mauris mattis, egestas tortor ut, semper nisl. Ut eros ipsum, bibendum eu condimentum pharetra, bibendum vel dui. Etiam porttitor risus quis venenatis rhoncus. Praesent vitae laoreet magna. Cras luctus orci vitae purus auctor, et tincidunt ex aliquet. Cras pharetra est non libero luctus, eu feugiat nisi eleifend. Praesent eu nulla lorem. Cras nec faucibus erat. Nullam lobortis tortor at ullamcorper viverra. Curabitur tristique ipsum a interdum consequat. In magna lacus, viverra sit amet massa ut, gravida laoreet neque. Nunc at malesuada est, vel tempus metus. Suspendisse id ante ullamcorper, imperdiet felis eget, scelerisque erat. Nunc luctus faucibus tortor at efficitur. Vivamus a felis sodales, mattis massa ac, blandit justo. Donec a elit ut magna dictum elementum. Mauris imperdiet velit at odio tempor lacinia. Nulla consectetur volutpat diam vitae condimentum.'
+//option1
 numWords = 1;
 for (let i=0; i < newString.length; i++) {
     let character = newString[i];
@@ -79,14 +80,18 @@ for (let i=0; i < newString.length; i++) {
     }
 }
 
-console.log(`El número de palabras es ${numWords}`)
+//option 2
+String2 = newString.split(" ")
+words= String2.length;
+
+console.log(`El número de palabras es ${numWords}`) //option 1
+console.log(`El número de palabras es ${words}`) //option 2
 
 //Bonus1.2
 let et ="et"
 let counter = 0;
 
 for (let i=1; i < newString.length; i++){
-
     if ( ((newString[i]+newString[i+1])==et) && (newString[i-1]===" ") && (newString[i+2]===" " )){
         counter++;
     }
